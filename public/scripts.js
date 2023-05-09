ec3cab81ce7ae2aecf803b1a5acc663409c0acd2
kaiq913 <77793337+kaiq913@users.noreply.github.com>
2023-05-08 22:22:07 -0400

    fix

--- public/scripts.js
+++ public/scripts.js
@@ -2,65 +2,102 @@
 // check out the coin-server example from a previous COMP 426 semester.
 // https://github.com/jdmar3/coinserver
 
-import {rpsls, rps} from './lib/rpsls.js';
+function playButton() {
+  // Get the game selector value
+  var gameSelector = document.querySelector('input[name="gameSelector"]:checked').value;
+  var rpsOpponentChoice = document.getElementById('rpsOpponentChoice').checked;
 
-function showButton(button) {
-    document.getElementById(button).style.display = "block";
-}
-function hideButton(button) {
-  document.getElementById(button).style.display = "none";
-}
-function getRadioValue(name) {
-    var radio = document.querySelector(`input[name=${name}]:checked`);
-    return radio.value;
-}
-async function playButton() {
-    var gameSelector = getRadioValue("gameSelector");
-    if (gameSelector === "rpsChosen") {
-        var opponentChoice = getRadioValue("rpsOpponentChoice")
-        if (opponentChoice === "yes") {
-            // CONTENT FOR RPS VS OPPONENT
-            const value = getRadioValue("rpsChoice");
-            const url = '/app/rps/play/' + value;
-            var response = await fetch(url);
-            var result = await response.json();
-            document.getElementById("resultText").innerHTML = `Player chose ${result.player}, Opponent chose ${result.opponent}, Result is ${result.result}.`;
-        } else {
-            // CONTENT FOR RPS WITH RANDOM DRAW
-            const rpsChoices = ["rock", "paper", "scissors"]
-            const computerChoice = rpsChoices[Math.floor(Math.random() * rpsChoices.length)];
-            const url = '/app/rps/play/' + computerChoice;
-            var response = await fetch(url);
-            var result = await response.json();
-            document.getElementById("resultText").innerHTML = `Player chose ${result.player}, Opponent chose ${result.opponent}, Result is ${result.result}.`;
-        }
-    } else {
-        var opponentChoice = getRadioValue("rpslsOpponentChoice")
-        if (opponentChoice === "yes") {
-            // CONTENT FOR RPSLS VS OPPONENT
-            const value = getRadioValue("rpslsChoice");
-            const url = '/app/rpsls/play/' + value;
-            var response = await fetch(url);
-            var result = await response.json();
-            document.getElementById("resultText").innerHTML = `Player chose ${result.player}, Opponent chose ${result.opponent}, Result is ${result.result}.`;
+  // Hide the game selector and opponent choice elements
+  document.querySelector('div:nth-of-type(1)').style.display = 'none';
+  document.querySelector('div:nth-of-type(2)').style.display = 'none';
 
-        } else {
-            // CONTENT FOR RPSLS WITH RANDOM DRAW
-            const rpslsChoices = ["rock", "paper", "scissors", "lizard", "spock"]
-            const computerChoice = rpslsChoices[Math.floor(Math.random() * rpslsChoices.length)];
-            const url = '/app/rpsls/play/' + computerChoice;
-            var response = await fetch(url);
-            var result = await response.json();
-            document.getElementById("resultText").innerHTML = `Player chose ${result.player}, Opponent chose ${result.opponent}, Result is ${result.result}.`;
-        }
-    }
-}
-function changeLabel(elementID, textReplacement) {
-    document.getElementById(elementID).innerHTML = textReplacement;
-}
+  // Show the appropriate choices element(s) based on the game selector value
+  if (gameSelector === 'rpsChosen') {
+    document.getElementById('rpsChoices').style.display = 'block';
+  } else if (gameSelector === 'rpslsChosen') {
+    document.getElementById('rpslsChoices').style.display = 'block';
+  }
 
-document.getElementById("playButton").addEventListener("click", playButton());
+  // Show the Play Again button
+  document.getElementById('playButton').innerHTML = '<button type="button" onclick="playAgain()">Play Again</button>';
+
+  // Hide the Result section (if visible)
+  //document.getElementById('resultHeader').style.display = 'none';
+  //document.getElementById('resultText').style.display = 'none';
+
+  // Get the player's move
+  var playerChoice;
+  if (gameSelector === 'rpsChosen') {
+    playerChoice = document.querySelector('input[name="rpsChoice"]:checked').value;
+  } else {
+    playerChoice = document.querySelector('input[name="rpslsChoice"]:checked').value;
+  }
+
+  // Get the opponent's move if checkbox is checked
+  //var opponentChoice;
+  //if (document.getElementById("rpsOpponentChoice").checked) {
+  //  opponentChoice = getOpponentChoice(gameSelector);
+  //} else {
+  //  opponentChoice = '';
+//  }
+
+// Determine the winner
+fetch('/app/rps/play/' + playerChoice)
+  .then(response => response.json())
+  .then(data => {
+    // Display the result
+    document.getElementById('resultHeader').style.display = 'block';
+    document.getElementById('resultText').innerHTML = data;
+  })
+  .catch(error => {
+    console.error(error);
+  });
+
+
+  // Display the result
+  document.getElementById('resultHeader').style.display = 'block';
+  document.getElementById('resultText').innerHTML = result;
+}
 
 function resetPage() {
-    window.location.reload();
+  // Reset radio buttons and checkbox
+  document.querySelector('input[name="gameSelector"][value="rpsChosen"]').checked = true;
+  document.getElementById("rpsOpponentChoice").checked = false;
+  document.querySelector('input[name="rpsChoice"][value="rock"]').checked = true;
+  document.querySelector('input[name="rpslsChoice"][value="rock"]').checked = true;
+
+  // Hide result
+  document.getElementById('resultHeader').style.display = 'none';
+  document.getElementById('resultText').innerHTML = '';
+}
+function getOpponentChoice(gameSelector) {
+  // Generate a random choice for the opponent
+  var choices = ['rock', 'paper', 'scissors'];
+  if (gameSelector === 'rpslsChosen') {
+    choices.push('lizard', 'spock');
+  }
+  var randomIndex = Math.floor(Math.random() * choices.length);
+  return choices[randomIndex];
+}
+
+function determineWinner(playerChoice, opponentChoice) {
+  // Determine the winner based on the choices
+  if (playerChoice === opponentChoice) {
+    return "It's a tie!";
+  } else if (
+    (playerChoice === 'rock' && opponentChoice === 'scissors') ||
+    (playerChoice === 'paper' && opponentChoice === 'rock') ||
+    (playerChoice === 'scissors' && opponentChoice === 'paper') ||
+    (playerChoice === 'rock' && opponentChoice === 'lizard') ||
+    (playerChoice === 'lizard' && opponentChoice === 'spock') ||
+    (playerChoice === 'spock' && opponentChoice === 'scissors') ||
+    (playerChoice === 'scissors' && opponentChoice === 'lizard') ||
+    (playerChoice === 'lizard' && opponentChoice === 'paper') ||
+    (playerChoice === 'paper' && opponentChoice === 'spock') ||
+    (playerChoice === 'spock' && opponentChoice === 'rock')
+  ) {
+    return 'You win!';
+  } else {
+    return 'You lose!';
+  }
 }
